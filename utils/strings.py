6de55c8c5d3452f684a55b8676f821f80f691821
kartikmandar <kartik4321mandar@gmail.com>
2024-07-24 19:31:47 +0530

    format help string in data ingestion

--- utils/strings.py
+++ utils/strings.py
@@ -4,7 +4,7 @@ HOME_HEADER_STRING = """
 <h1>Welcome to the Stingray Explorer Dashboard</h1>
 """
 
-WELCOME_MESSAGE_STRING = """
+HOME_WELCOME_MESSAGE_STRING = """
 <div>
     <p>This dashboard is designed to provide a comprehensive toolset for X-ray astronomy data analysis. Here are the main features:</p>
     <ul>
@@ -63,7 +63,7 @@ WELCOME_MESSAGE_STRING = """
 </div>
 """
 
-FOOTER_STRING = """
+HOME_FOOTER_STRING = """
     <div>
         <p>Stingray Explorer Dashboard</p>
         <p>&copy; 2021 Kartik Mandar</p>
@@ -73,7 +73,7 @@ FOOTER_STRING = """
 
 # Tabs in Main Area of Home Page
 
-STINGRAY_TAB_STRING = """
+HOME_STINGRAY_TAB_STRING = """
 <h2> Stingray: Next-Generation Spectral Timing </h2>
 
 ![Stingray-logo](../assets/images/stingray_logo_minimised.png)
@@ -130,7 +130,7 @@ Windows uses an internal 32-bit representation for int. This might create numeri
 <br></br>
 """
 
-HOLOVIZ_TAB_STRING = """
+HOME_HOLOVIZ_TAB_STRING = """
 <h2> High-level tools to simplify visualization in Python </h2>
 
 ![HoloViz-logo](../assets/images/holoviz_logo_minimised.png)
@@ -185,7 +185,7 @@ HoloViz tools provide extensive support for Jupyter notebooks, as well as for st
 <br/>
 """
 
-DASHBOARD_TAB_STRING = """
+HOME_DASHBOARD_TAB_STRING = """
 <h2> Stingray Explorer </h2>
 
 ![Stingray-Explorer-logo](../assets/images/stingray_explorer_minimised.png)
@@ -195,18 +195,149 @@ This dashboard is designed to provide a comprehensive toolset for X-ray astronom
 <br/>
 """
 
-OUTPUT_BOX_STRING = """
+HOME_OUTPUT_BOX_STRING = """
 This is the output container. It will display the output of the analysis tools.
 It may contain what EventLists are made, what lightcurves are generated, what power spectra are calculated, etc. It can also be used to preview the loaded EventList. 
 """
 
 
-WARNING_BOX_STRING = """
+HOME_WARNING_BOX_STRING = """
 This is the warning container. It will display any warnings or errors that occur during the analysis process.
 """
 
-HELP_BOX_STRING = """
+HOME_HELP_BOX_STRING = """
 This is the helpbox container. It will display any help or documentation for the analysis tools.
 It will also have links to the documentation and tutorials for the tools.
 And have a brief description of the physics behind the analysis. 
+"""
+
+
+# This section contains strings used in the DataIngestion.py
+
+LOADING_DATA_HELP_BOX_STRING = """
+<h2> Loading Tab </h2>
+
+### Functionality
+The "Loading" tab allows you to load, save, delete, and preview event data files. Here is a detailed explanation of each component and its functionality:
+
+- **File Selector**: Select files to load into the event data list.
+- **Enter File Names**: Specify custom names for the loaded files. If left blank, the names will be derived from the file paths.
+- **Enter Formats**: Specify the formats of the files being loaded. If left blank, the default format is used.
+- **Use default format**: Check this to use the default format ('ogip' for loading and 'hdf5' for saving).
+- **Load Event Data**: Load the selected files into the event data list.
+- **Save Loaded Data**: Save the loaded event data files to the specified directory.
+- **Delete Selected Files**: Delete the selected files from the file system.
+- **Preview Loaded Files**: Preview the contents of the loaded event data files.
+
+### Precautions
+- Ensure the file contains at least a 'time' column when loading event data.
+- When specifying custom names or formats, ensure that the number of names/formats matches the number of files selected.
+- Deleting files with the ".py" extension is not allowed to prevent accidental deletion of script files.
+
+### Examples
+#### Loading an Event List
+```python
+from stingray import EventList
+ev = EventList.read('events.fits', 'ogip')
+print(ev.time)
+```
+
+### Saving an Event List
+```python
+ev.write("events.hdf5", "hdf5")
+```
+
+## Creation Tab
+
+### Functionality
+
+The "Creation" tab allows you to create new event lists or simulate event lists from light curves. Here is a detailed explanation of each component and its functionality:
+
+- **Photon Arrival Times**: Enter photon arrival times in seconds from a reference MJD.
+- **MJDREF**: Enter the MJD reference for the photon arrival times.
+- **Energy (optional)**: Enter the energy values associated with the photons.
+- **GTIs (optional)**: Enter the Good Time Intervals (GTIs) for the event list.
+- **Event List Name**: Specify a name for the new event list.
+- **Create Event List**: Create a new event list with the specified parameters.
+
+### Simulation of Event Lists
+
+- **Number of Time Bins**: Specify the number of time bins for the simulation.
+- **Max Counts per Bin**: Specify the maximum counts per bin.
+- **Delta Time (dt)**: Specify the delta time for the light curve.
+- **Method**: Choose between "Standard Method" and "Inverse CDF Method" for simulating event lists.
+- **Simulated Event List Name**: Specify a name for the simulated event list.
+- **Simulate Event List**: Simulate an event list using the specified parameters.
+
+### Precautions
+
+- Ensure that photon arrival times and MJDREF are provided when creating an event list.
+- When simulating event lists, ensure that the provided parameters (e.g., number of time bins, max counts per bin) are reasonable to avoid excessively large or small event lists.
+
+### Examples
+
+#### Creating an Event List
+
+```python
+from stingray import EventList
+times = [0.5, 1.1, 2.2, 3.7]
+mjdref = 58000.
+energy = [0., 3., 4., 20.]
+gti = [[0, 4]]
+ev = EventList(times, gti=gti, energy=energy, mjdref=mjdref)
+print(ev.time)
+```
+
+#### Simulating an Event List from a Light Curve
+```python
+from stingray import EventList, Lightcurve
+times = np.arange(3)
+counts = np.floor(np.random.rand(3) * 5)
+lc = Lightcurve(times, counts, skip_checks=True, dt=1.)
+ev = EventList.from_lc(lc)
+print(ev.time)
+```
+
+## Stingray Documentation References
+
+### Creating EventList from Photon Arrival Times
+
+```python
+from stingray import EventList
+times = [0.5, 1.1, 2.2, 3.7]
+mjdref = 58000.
+ev = EventList(times, mjdref=mjdref)
+print(ev.time)
+```
+
+### Transforming a Lightcurve into an EventList
+```python
+from stingray import EventList, Lightcurve
+times = np.arange(3)
+counts = np.floor(np.random.rand(3) * 5)
+lc = Lightcurve(times, counts, skip_checks=True, dt=1.)
+ev = EventList.from_lc(lc)
+print(ev.time)
+```
+
+### Simulating EventList from Lightcurve
+```python
+from stingray import EventList, Lightcurve
+times = np.arange(50)
+counts = np.floor(np.random.rand(50) * 50000)
+lc = Lightcurve(times, counts, skip_checks=True, dt=1.)
+ev = EventList()
+ev.simulate_times(lc)
+print(ev.time)
+```
+
+### Loading and Writing EventList Objects
+```python
+from stingray import EventList
+ev = EventList.read('events.fits', 'ogip')
+ev.write("events.hdf5", "hdf5")
+```
+
+<br/>
+
 """
